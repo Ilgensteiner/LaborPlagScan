502651eb78841525ba5f57915b4e83dca708b52c
Ilgensteiner <erikilgenstein@gmail.com>
2023-05-06 01:17:28 +0200

    GUI 0.3 Update

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import filedialog, ttk, PhotoImage
+from PIL import Image, ImageTk
+import pygame
 
 import PlagiatScanner
 
@@ -15,8 +17,8 @@ def on_leave(e):
 class GUI:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("900x400")
-        self.root.title("PlagScan GUI")
+        self.root.geometry("600x300")
+        self.root.title("PlagScan for Java-Labore")
         self.root.configure(bg='#0a3055')
 
         zip_button = tk.Button(self.root, text="ZIP auswählen", command=self.select_zip)
@@ -37,7 +39,31 @@ class GUI:
         self.info_textline = tk.Label(self.root, text="")
         self.info_textline.pack()
 
+        self.name_textline = tk.Button(self.root, text="© 2023 by Erik Ilgenstein", bg="#0a3055", fg="grey",
+                                       font=("Calibri", 10), border=0, command=self.hidden_button_click,
+                                       activebackground="#0a3055", activeforeground="grey", takefocus=0)
+        self.name_textline.place(relx=1.0, rely=1.0, anchor="se")
+        # easteregg
+        self.clicks = 0
+        self.music_playing = False
+        pygame.mixer.init()
+
         self.progressbar = ttk.Progressbar(self.root, orient="horizontal", length=200, mode="determinate")
+
+    def hidden_button_click(self):
+        # Zähle Klicks
+        self.clicks += 1
+
+        # Wenn 5 Klicks erreicht sind, starte Musik
+        if self.clicks >= 5:
+            # toggle Music
+            if not self.music_playing:
+                pygame.mixer.music.load('resources/UniversalAdobePatcherMusic.mp3')
+                pygame.mixer.music.play()
+                self.music_playing = True
+            else:
+                pygame.mixer.music.pause()
+                self.music_playing = False
 
     def set_progressbar_start(self, value: int):
         self.progressbar['maximum'] = value
