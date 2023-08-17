d001d2cd8c99a06c0069a5990ed89dfded5e5bc1
Ilgensteiner <108222413+Ilgensteiner@users.noreply.github.com>
2023-08-17 14:00:06 +0200

    Update: Rename Test-Folder

--- tests/test_plagiatScanner.py
+++ tests/test_plagiatScanner.py
@@ -23,16 +23,16 @@ class TestPlagScan(unittest.TestCase):
         warnings.simplefilter("ignore", category=DeprecationWarning)
 
         # Entpacken der ZIP-Datei
-        self.students_folder = extract_zip('test/resources/Test_Labor.zip')
+        self.students_folder = extract_zip('tests/resources/Test_Labor.zip')
         self.students_folder = unpackZipFiles(self.students_folder, self.gui_mock)
 
     def tearDown(self):
         # Testdateien aufräumen
-        last_result_path = 'test/resources/last_result.json'
+        last_result_path = 'tests/resources/last_result.json'
         if os.path.exists(last_result_path):
             os.remove(last_result_path)
-        if os.path.exists("test/resources/Test_Labor"):
-            shutil.rmtree("test/resources/Test_Labor")
+        if os.path.exists("tests/resources/Test_Labor"):
+            shutil.rmtree("tests/resources/Test_Labor")
 
         # Arbeitsverzeichnis zurücksetzen
         os.chdir(self.original_cwd)
@@ -53,9 +53,9 @@ class TestPlagScan(unittest.TestCase):
                 plagscan(self.students_folder, self.gui_mock)
 
                 # Überprüfen Sie das Ergebnis
-                with open('test/resources/last_result.json', 'r') as f:
+                with open('tests/resources/last_result.json', 'r') as f:
                     result = json.load(f)
-                with open('test/resources/sollResult.json', 'r') as f:
+                with open('tests/resources/sollResult.json', 'r') as f:
                     expected = json.load(f)
 
                 self.assertEqual(result, expected)
@@ -70,7 +70,7 @@ class TestPlagScan(unittest.TestCase):
                 msgbox_mock.return_value = None
 
                 # Testaufruf mit leerem Ordner
-                empty_folder_path = "test/resources/Empty_Labor"  # Pfad zum leeren Testordner
+                empty_folder_path = "tests/resources/Empty_Labor"  # Pfad zum leeren Testordner
                 plagscan(empty_folder_path, self.gui_mock)
 
                 # Überprüfen Sie das erwartete Verhalten (kann je nach Anforderungen angepasst werden)
@@ -86,7 +86,7 @@ class TestPlagScan(unittest.TestCase):
                 msgbox_mock.return_value = None
 
                 # Testaufruf mit einem Ordner ohne Java-Dateien
-                no_java_folder_path = "test/resources/NoJars_Labor/Updated_Labor/extracted"  # Pfad zum Testordner ohne Java-Dateien
+                no_java_folder_path = "tests/resources/NoJars_Labor/Updated_Labor/extracted"  # Pfad zum Testordner ohne Java-Dateien
                 plagscan(no_java_folder_path, self.gui_mock)
 
                 # Überprüfen Sie das erwartete Verhalten (kann je nach Anforderungen angepasst werden)
@@ -94,7 +94,7 @@ class TestPlagScan(unittest.TestCase):
 
     def save_mock(self, sorted_plagiat_dict):
         # Ersetzen Sie den Speicherpfad und speichern Sie das Ergebnis
-        path = 'test/resources/'
+        path = 'tests/resources/'
         save_auswertung_to_file(sorted_plagiat_dict, path=path)
 
 
