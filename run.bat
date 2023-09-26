2e1c84609bb2ac0a937b5a17a6c917f174c09838
Ilgensteiner <108222413+Ilgensteiner@users.noreply.github.com>
2023-09-26 22:32:32 +0200

    Update: Add venv and Auto-Install

--- run.bat
+++ run.bat
@@ -1,4 +1,23 @@
 @echo off
+
+echo "Labor PlagScanner by Erik Ilgenstein"
+
+rem 1. Prüfe die Python Version
+python --version | findstr /i "3.[1-9]" > nul
+if errorlevel 1 (
+    echo "Die Python Version muss > 10 sein."
+    pause
+    exit /b 1
+)
+
+rem 2. Aktiviere das venv
+if not exist .venv (
+	echo "Umgebung wird vorbereitet..."
+    python -m venv .venv
+)
+call .venv\Scripts\activate
+
+rem Restliche Schritte wie gehabt
 python scripts/check_requirements.py requirements.txt
 if errorlevel 1 (
     echo Installing missing packages...
--- run.sh
+++ run.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-python scripts/check_requirements.py requirements.txt
-if [ $? -eq 1 ]
-then
-    echo Installing missing packages...
-    pip install -r requirements.txt
+
+echo "Labor PlagScanner by Erik Ilgenstein"
+
+# 1. Prüfe die Python Version
+python --version | grep -q "^3\.[1-9]\.[0-9]$"
+if [[ $? -eq 0 ]]; then
+  echo "Die Python Version muss mindestens 3.10 sein."
+  exit 1
 fi
-nohup python3 run.py &
+
+# 2. Aktiviere das venv
+if [ ! -d .venv ]; then
+  echo "Umgebung wird vorbereitet..."
+  python -m venv .venv
+fi
+source .venv/bin/activate
+
+# Restliche Schritte wie gehabt
+python scripts/check_requirements.py requirements.txt
+if [[ $? -eq 1 ]]; then
+  echo "Fehlende Pakete werden installiert..."
+  pip install -r requirements.txt
+fi
+start pythonw run.py
 app.Activate()
