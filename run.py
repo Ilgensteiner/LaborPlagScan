2b3a0ee031761f48563d014ca2396a62b3587878
Ilgensteiner <108222413+Ilgensteiner@users.noreply.github.com>
2023-10-14 01:15:34 +0200

    Update: Add excepthook

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import sys
+
 from laborPlagScan import main
+import laborPlagScan.basicConfig as basicConfig
+
 
 if __name__ == '__main__':
+    sys.excepthook = basicConfig.handle_exception
     main.run()
